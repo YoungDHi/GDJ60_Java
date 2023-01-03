4e3a8f02d3cce9834a25fece165ab2eb707fef0b
YoungDHi <sm930219@naver.com>
2023-01-03 14:18:40 +0900

    test

--- src/com/iu/s4/objects1/ex1/Main.java
+++ src/com/iu/s4/objects1/ex1/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("main method 실행");
+		System.out.println("start");
 		MenuController mc = new MenuController();
 		
 		mc.start();
